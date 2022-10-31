4ac7e1f531cdfd223ab84a82d8c6dc498dd07efa
Minel Drda <mineldrdait@gmail.com>
2022-10-31 12:38:56 +0100

    Created custom request hook

---
+++ frontend/src/shared/hooks/useHttp.js
@@ -0,0 +1,51 @@
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+
+export const useHttp = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = 'GET', body = null, headers = {}) => {
+      setLoading(true);
+
+      const httpAbort = new AbortController();
+      activeRequests.current.push(httpAbort);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbort.signal,
+        });
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (error) {
+        setError(error.essage);
+      }
+      setLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeRequests.current.forEach((abortController) =>
+        abortController.abort()
+      );
+    };
+  }, []);
+
+  return { loading, error, sendRequest, clearError };
+};
